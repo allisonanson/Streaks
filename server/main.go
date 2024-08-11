b58a72c307495236c78a2e195e8bf134ea910ceb
Richard Sullivan <richard.m.sullivan2@gmail.com>
2024-08-11 12:39:12 -0500

    feat: add a hello world response to th default route

--- server/main.go
+++ server/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io"
+    "log"
+    "net/http"
+)
 
 func main() {
-    fmt.Println("Hello World!")
+    // create a handler for root
+    http.HandleFunc("/", helloHandler)
+
+    // start server
+    fmt.Println("Starting Server")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+    // return hello world
+    io.WriteString(w, "<h1> Hello, World! </h1>")
 }
